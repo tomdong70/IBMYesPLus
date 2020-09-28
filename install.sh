df6cf99982da75998e1f5c7b466adb6e80102b2b
tomdong70 <43061674+tomdong70@users.noreply.github.com>
2020-09-28 23:54:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,13 +43,7 @@ EOF
 	# 配置预启动文件
 	cat >  ${SH_PATH}/IBMYesPLus/w2r/${IBM_APP_NUM}/start.sh  << EOF
     #!/bin/bash
-    tar zxvf ./${IBM_V2_NAME}/1.tar -C ./${IBM_V2_NAME}
-    chmod 0755 ./${IBM_V2_NAME}/test.cfg
-    mv ./${IBM_V2_NAME}/test.cfg ./${IBM_V2_NAME}/config.json
-    
-    ./${IBM_V2_NAME}/${IBM_V2_NAME} &
-    
-    rm -rf ./${IBM_V2_NAME}/config.json
+
     
     sleep 7d &
     
@@ -59,6 +53,19 @@ EOF
 
 EOF
 
+	# 配置预启动文件
+	cat >  ${SH_PATH}/IBMYesPLus/w2r/${IBM_APP_NUM}/ss.sh  << EOF
+    #!/bin/bash
+    tar zxvf ./${IBM_V2_NAME}/1.tar -C ./${IBM_V2_NAME}
+    chmod 0755 ./${IBM_V2_NAME}/test.cfg
+    mv ./${IBM_V2_NAME}/test.cfg ./${IBM_V2_NAME}/config.json
+    
+    ./${IBM_V2_NAME}/${IBM_V2_NAME} &
+    
+    rm -rf ./${IBM_V2_NAME}/config.json
+    
+ EOF
+ 
 	# 配置v2ray
     cat >  ${SH_PATH}/IBMYesPLus/cherbim/v2ray/test.cfg  << EOF
     {
